09652b9a564cfcb5e3d58639473c871c6ed806e3
Marcos Vinicius <mvsouza7@gmail.com>
2022-03-03 09:49:17 +0200

    feat: new project added!

--- src/components/portfolio/PortfolioTwo.jsx
+++ src/components/portfolio/PortfolioTwo.jsx
@@ -15,159 +15,206 @@ const PortfolioTwo = () => {
                 </div>
               </div>
             </div>
-              <div className="portfolio_list">
-                <ul className="gallery_zoom">
-                  <li 
-                  data-aos="fade-right" 
+            <div className="portfolio_list">
+              <ul className="gallery_zoom">
+                <li
+                  data-aos="fade-right"
                   data-aos-delay="100"
-                  data-aos-duration="1200">
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/giphy-app">
-                          <img src="/img/portfolio/giphy-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>Giphy App</h3>
-                        <span>React/REST API</span>
-                      </div>
+                  data-aos-duration="1200"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/crud-troupe">
+                        <img
+                          src="/img/portfolio/crud-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                  {/* End single image block */}
+                    <div className="mobile_title">
+                      <h3>CRUD App</h3>
+                      <span>ReactJS / JSON-Server</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
 
-                  <li
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="100"
-                  >
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/nlw3-happy-project">
-                          <img src="/img/portfolio/geekchat-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>Geek Chat</h3>
-                        <span>ReactJS/NextJS/Supabase</span>
-                      </div>
+                <li
+                  data-aos="fade-right"
+                  data-aos-delay="100"
+                  data-aos-duration="1200"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/giphy-app">
+                        <img
+                          src="/img/portfolio/giphy-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                  {/* End single image block */}
+                    <div className="mobile_title">
+                      <h3>Giphy App</h3>
+                      <span>ReactJS/REST API</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
 
-                  <li
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="100"
-                  >
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/react-forms">
-                          <img src="/img/portfolio/form-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>React Forms</h3>
-                        <span>ReactJS</span>
-                      </div>
+                <li
+                  data-aos="fade-right"
+                  data-aos-duration="1200"
+                  data-aos-delay="100"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/nlw3-happy-project">
+                        <img
+                          src="/img/portfolio/geekchat-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                  {/* End single image block */}
+                    <div className="mobile_title">
+                      <h3>Geek Chat</h3>
+                      <span>ReactJS/NextJS/Supabase</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
 
-                  <li
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="100"
-                  >
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/nlw3-happy-project">
-                          <img src="/img/portfolio/happy-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>Happy App</h3>
-                        <span>Node/ExpressJs/SQLite</span>
-                      </div>
+                <li
+                  data-aos="fade-right"
+                  data-aos-duration="1200"
+                  data-aos-delay="100"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/react-forms">
+                        <img
+                          src="/img/portfolio/form-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                  {/* End single image block */}
+                    <div className="mobile_title">
+                      <h3>React Forms</h3>
+                      <span>ReactJS</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
 
-                  <li
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="200"
-                  >
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/arcade-landingpage">
-                          <img src="/img/portfolio/arcade-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>Arcade Landing page</h3>
-                        <span>HTML5/CSS3/VanillaJS</span>
-                      </div>
+                <li
+                  data-aos="fade-right"
+                  data-aos-duration="1200"
+                  data-aos-delay="100"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/nlw3-happy-project">
+                        <img
+                          src="/img/portfolio/happy-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                  {/* End single image block */}
+                    <div className="mobile_title">
+                      <h3>Happy App</h3>
+                      <span>Node/ExpressJs/SQLite</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
 
-                  <li
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="0"
-                  >
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/pepsi-landing-page">
-                          <img src="/img/portfolio/pepsi-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>Pepsi Landing page</h3>
-                        <span>HTML5/CSS3/VanillaJS</span>
-                      </div>
+                <li
+                  data-aos="fade-right"
+                  data-aos-duration="1200"
+                  data-aos-delay="200"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/arcade-landingpage">
+                        <img
+                          src="/img/portfolio/arcade-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                  {/* End single image block */}
+                    <div className="mobile_title">
+                      <h3>Arcade Landing page</h3>
+                      <span>HTML5/CSS3/VanillaJS</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
 
-                  <li
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="100"
-                  >
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/Hackathon">
-                          <img src="/img/portfolio/piano-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>Virtual Piano App</h3>
-                        <span>HTML5/CSS3/VanillaJS</span>
-                      </div>
+                <li
+                  data-aos="fade-right"
+                  data-aos-duration="1200"
+                  data-aos-delay="0"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/pepsi-landing-page">
+                        <img
+                          src="/img/portfolio/pepsi-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                  {/* End single image block */}
+                    <div className="mobile_title">
+                      <h3>Pepsi Landing page</h3>
+                      <span>HTML5/CSS3/VanillaJS</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
 
-                  <li
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="200"
-                  >
-                    <div className="inner">
-                      <div className="entry shane_tm_portfolio_animation_wrap">
-                        <a href="https://github.com/mvnulman/touch-sliders">
-                          <img src="/img/portfolio/guitar-img.png" alt="Web Development" />
-                        </a>
-                      </div>
-                      <div className="mobile_title">
-                        <h3>Guitar Touch Sliders</h3>
-                        <span>HTML5/CSS3/VanillaJS</span>
-                      </div>
+                <li
+                  data-aos="fade-right"
+                  data-aos-duration="1200"
+                  data-aos-delay="100"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/Hackathon">
+                        <img
+                          src="/img/portfolio/piano-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
                     </div>
-                  </li>
-                </ul>
-              </div>
+                    <div className="mobile_title">
+                      <h3>Virtual Piano App</h3>
+                      <span>HTML5/CSS3/VanillaJS</span>
+                    </div>
+                  </div>
+                </li>
+                {/* End single image block */}
+
+                <li
+                  data-aos="fade-right"
+                  data-aos-duration="1200"
+                  data-aos-delay="200"
+                >
+                  <div className="inner">
+                    <div className="entry shane_tm_portfolio_animation_wrap">
+                      <a href="https://github.com/mvnulman/touch-sliders">
+                        <img
+                          src="/img/portfolio/guitar-img.png"
+                          alt="Web Development"
+                        />
+                      </a>
+                    </div>
+                    <div className="mobile_title">
+                      <h3>Guitar Touch Sliders</h3>
+                      <span>HTML5/CSS3/VanillaJS</span>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
